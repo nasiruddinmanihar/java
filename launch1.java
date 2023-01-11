9765e3f71206a2d1ce8625523409def9e9a1856e
nasiruddinmanihar <nasirmanihar0302@gmail.com>
2023-01-11 22:21:47 +0530

    first commit

---
+++ launch1.java
@@ -0,0 +1,24 @@
+
+public class launch1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		final int a=5;
+//		System.out.println(a++);
+		
+//		StringBuffer s=new StringBuffer("Nasir");
+//		s.append('s');
+//		System.out.println(s);
+//		
+//		String s2=new String("Nasir");
+//		 s2.concat(" Manihar");
+//		System.out.println(s2);
+		
+		StringBuffer s=new StringBuffer();
+		System.out.println(s.capacity());
+		
+		
+
+	}
+
+}
